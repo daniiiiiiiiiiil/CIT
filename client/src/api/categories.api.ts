21a4ef4619de5c8ee86540393838bca999a2c04e
Danil <Napalkov-danil@list.ru>
2026-05-18 00:19:55 +0300

    deploy

--- client/src/api/categories.api.ts
+++ client/src/api/categories.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { Category, User } from "../types/categories.types";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL;
 
 const getToken = () => localStorage.getItem("token");
 const getHeaders = () => ({ Authorization: `Bearer ${getToken()}` });
