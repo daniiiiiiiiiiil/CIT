458bb72fd4f170786982c3969633a06282a63f4e
Danil <Napalkov-danil@list.ru>
2026-05-20 17:16:26 +0300

    api

--- server/server.js
+++ server/server.js
@@ -12,8 +12,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    credentials: true
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://5.42.121.83:5173',
+        'http://5.42.121.83:5174'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
