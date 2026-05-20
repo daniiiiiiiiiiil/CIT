cce62165ecae970f4549543b9e0f5691b491fb90
Danil <Napalkov-danil@list.ru>
2026-05-20 17:50:29 +0300

    api

--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ app.use(cors({
         'http://localhost:5173',
         'http://localhost:5174',
         'http://5.42.121.83:5173',
-        'http://5.42.121.83:5174'
+        'http://5.42.121.83:5174',
+        'http://5.42.121.83.nip.io:5173'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
